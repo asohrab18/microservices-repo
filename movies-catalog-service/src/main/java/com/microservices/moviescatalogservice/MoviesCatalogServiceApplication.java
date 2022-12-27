77e022fb3441423a68c7f3df6114ae1e482f1523
Sohrab Alam <sohrab.alam86@gmail.com>
2022-12-27 12:30:19 +0530

    3. Setting timeout on RestTemplate to partly avoid slowness

--- movies-catalog-service/src/main/java/com/microservices/moviescatalogservice/MoviesCatalogServiceApplication.java
+++ movies-catalog-service/src/main/java/com/microservices/moviescatalogservice/MoviesCatalogServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -12,7 +13,9 @@ public class MoviesCatalogServiceApplication {
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+		factory.setConnectTimeout(3000);
+		return new RestTemplate(factory);
 	}
 
 	public static void main(String[] args) {
